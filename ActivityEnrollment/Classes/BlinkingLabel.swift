07560eb8f4e0de42ab2a81f47dde2c73d6b35a2a
sam shi <sam.shi@activenetwork.com>
2019-03-04 16:42:26 +0800

    update swift 4.0

--- ActivityEnrollment/Classes/BlinkingLabel.swift
+++ ActivityEnrollment/Classes/BlinkingLabel.swift
@@ -9,7 +9,7 @@ import UIKit
 
 public class BlinkingLabel : UILabel {
     public func startBlinking() {
-        let options : UIViewAnimationOptions = UIViewAnimationOptions(arrayLiteral: UIViewAnimationOptions.repeat, UIViewAnimationOptions.autoreverse)
+        let options : UIView.AnimationOptions = UIView.AnimationOptions(arrayLiteral: UIViewAnimationOptions.repeat, UIViewAnimationOptions.autoreverse)
         UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
             self.alpha = 0
         }, completion: nil)
